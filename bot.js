03ead159f886787e2b2bce6d9522083c626d883e
ayman2almonster <42792992+ayman2almonster@users.noreply.github.com>
2018-11-27 00:31:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -398,22 +398,6 @@ client.on('message', msg => {
   }
 });
 
-			  
-client.on('guildCreate', guild => {
-         const embed = new Discord.RichEmbed()
-     .setColor("RED")
-     .setTitle('Click Here To Add Bot .!')
-     .setURL('https://discordapp.com/api/oauth2/authorize?client_id=509459708281749515&permissions=8&scope=bot`);
-  .setDescription(`**
-  New Server Add ALmonster bot ✅
-Server name: __${guild.name}__
-Server owner: __${guild.owner}__
-Server id: __${guild.id}__ 
-Server Count: __${guild.memberCount}__**`);
-client.channels.get("467833183254347797").sendEmbed(embed)
-});
-
- 
 const prefix = "$"
 client.on('message', async msg => { 
 	if (msg.author.bot) return undefined;
